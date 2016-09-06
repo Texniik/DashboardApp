261f6f021f65f8981e1c3410b3e6e2727a13ca7a
Haris Zenovic <zenovicharis@live.com>
2016-09-06 20:10:41 +0200

    Before Azure

--- DashboardApp/Global.asax.cs
+++ DashboardApp/Global.asax.cs
@@ -5,15 +5,16 @@ using System.Web.Routing;
 
 namespace DashboardApp
 {
-  public class MvcApplication : HttpApplication
-  {
-    protected void Application_Start()
+    public class MvcApplication : HttpApplication
     {
-      AreaRegistration.RegisterAllAreas();
+        protected void Application_Start()
+        {
+            BundleTable.EnableOptimizations = true;
+            AreaRegistration.RegisterAllAreas();
 
-      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-      RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
     }
-  }
 }
